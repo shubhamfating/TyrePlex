6a2186598edc407b213b56a6bcbc2131fde4b0ed
Shubham Fating <shubhamfating5@gmail.com>
2024-08-28 21:54:18 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'E:/tyreplexassignment/tyreplexassignment/src/styles/main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './styles/main.css';
 
 
 
